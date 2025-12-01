b1b6228b881b68d6b0abea30643f99b32e8b3fff
annapotta <annapcan@gmail.com>
2025-12-01 07:56:31 -0500

    Update split-convert.py

--- split-convert.py
+++ split-convert.py
@@ -1,3 +1,144 @@
+5.3 EXERCISES
+# finding a value in a list
+import random
+
+list = []
+
+for i in range(len(list) + 10):
+    list.append(random.randrange(1, 51))
+
+print(f"List: {list}")
+user_num = int(input("Value to find: "))
+
+for number in list:
+    if number == user_num:
+        print(f"{user_num} is in the list.")
+
+# finding the largest value in a list
+import random
+
+list = []
+max_number = 0
+
+for i in range(len(list) + 10):
+    list.append(random.randrange(1, 101))
+
+for n in list: 
+    if n > max_number:
+        max_number = n
+
+print(f"Array: {list}")
+print(f"Biggest: {max_number}.")
+
+# random grades
+import random 
+
+name = input("Name (first last): ")
+filename = input("Filename: ")
+
+grades = []
+for i in range(5):
+    grades.append(random.randrange(1, 100))
+
+total = 0 
+print("Here are your randomly-selected grades (hope you pass):")
+for i in range(5):
+    print(f"Grade {i + 1}: {grades[i]}")
+    total += grades[i]
+
+average = total / 5
+print(f"Your average: {average}.")
+
+if average < 50:
+    print("You DID NOT PASS. FAIL!!!!")
+else: 
+    print("Congrats, you passed!")
+
+# random num, count how many times appears in list
+import random
+
+list = []
+
+for i in range(len(list) + 10):
+    list.append(random.randrange(1, 51))
+
+print(f"List: {list}") 
+user_num = int(input("Value to find: "))
+
+count = 0
+for number in list:
+    if number == user_num:
+        print(f"{user_num} is in the list.")
+        count += 1
+
+print(f"{user_num} was found {count} times.")
+
+# locating largest number in a list
+import random
+
+list = []
+max_number = 0
+
+for i in range(len(list) + 10):
+    list.append(random.randrange(1, 101))
+
+for n in list: 
+    if n > max_number:
+        max_number = n
+
+pos = 0
+for i in list:
+    if i != max_number: 
+        pos += 1
+    else:
+        break
+
+print(f"Array: {list}")
+print(f"Biggest: {max_number}.")
+print(f"It is at index {pos}.")
+
+# random num, search for a prompted integer
+import random
+
+list = []
+
+for i in range(len(list) + 10):
+    list.append(random.randrange(1, 51))
+
+print(f"List: {list}")
+user_num = int(input("Value to find: "))
+
+for number in list: 
+    if number == user_num:
+        print(f"{user_num} is in the list.")
+        break
+    else:
+        print(f"{user_num} is not in the list.")
+        break
+
+# random num, search for prompted int and locate it
+import random
+
+list = []
+
+for i in range(len(list) + 10):
+    list.append(random.randrange(1, 51))
+
+print(f"List: {list}")
+user_num = int(input("Value to find: "))
+
+found_count = 0
+
+for i, number in enumerate(list):
+    if number == user_num:
+        print(f"{user_num} is at index {i}.")
+        found_count += 1
+
+if found_count == 0: 
+    print(f"{user_num} is not in the list.")
+
+5.4 EXERCISES
+
 # # 1 
 # animals_str = "monkey bat cat dog"
 
@@ -80,3 +221,78 @@ for dim in dimensions.split(" - "):
     dimensions_list.append([int(dim_x), int(dim_y)])
 
 print(dimensions_list)
+
+
+--------------------------------------------------------
+
+PRACTICE QUIZ SOLUTIONS
+
+# 1. Ask the user for 1000 words using a loop
+# and put them into a list. 
+# The words will be entred one at a time.
+words = []
+
+for _ in range(1000):
+    word = input("enter a word: ")
+    words.append(word)
+
+
+
+# 2. Given that list above, ask the user to 
+# enter a word to search for in the list. 
+# The program will print out the index of the first occurance of that word.
+
+target = input("what word to look for: ")
+for i in range(len(words)):
+    if words[i] == target:
+        print(i)
+        break
+
+
+# 3. Ask the user to enter numbers on a single line separated by a space.
+# Split that string and create a loop that will convert each of the inputted
+# numbers to an integer either replacing the string or create a new list
+# to hold the integer values.
+user_numbers = input("Enter numbers on one line: ")
+number_strings = user_numbers.split(" ")
+
+numbers = []
+for n in number_strings:
+    n = int(n)
+    numbers.append(n)
+
+
+# 4. Assuming that a list containing numbers already exists - the list is called numbers.
+# Search the list for the index values of all the negative numbers.
+# Your program should save all the index values into another list called found.
+
+found = []
+for i in range(len(numbers)):
+    n = numbers[i]
+    if n < 0:
+        found.append(i)
+
+SHORT ANSWERS
+# practice quiz short answer
+# 1. Create a list that will store the even numbers from 0–10 inclusive.
+list = []
+for n in range(0, 11, 2):
+    list.append(n)
+
+# 2. How does one print out the second list element in a list called friends?
+friends = ["sally", 'bob']
+print(friends[1])
+
+# 3. what are two ways to print out the last element in a list called numbers?
+numbers = [1, 2]
+print(numbers[-1])
+print(numbers[len(numbers) - 1])
+
+# 4. Given a list called numbers containing [76, 55, 34, 24], how do you slice the list to include only the middle two?
+middle_two = numbers[1:3]
+
+# 5. Given a list called words, how do you get a slice of only the last three words?
+words = ['hi']
+last_three = words[-3:]
+# in this negative indexing, -3 is the start point (third last), and if you omit the end, you go all the way to the end of the list
+# start at the third-last word and keep going until end of list.
